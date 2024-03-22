a8651be9339517e50ac4c41cedc5c12f7558f582
Nelson Sandoval <7774562+nvsv-u@users.noreply.github.com>
2024-03-22 08:57:56 -0300

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -9,6 +9,9 @@
 #		  mundo 44
 #La salida debe ser
 #         ('mundo', 44, 20, 90, 'hola', 'mundo', 44)
-t = input()
-m = input()
-print(m)
+t = input().split()
+m = input().split()
+
+salida = tuple(m + t + m)
+
+print(salida)
